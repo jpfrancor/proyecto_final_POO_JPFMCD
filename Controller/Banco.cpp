d0aaa386c94b27988c5b5700e6683e89880f1fcf
Your Name <your@email.com>
2025-11-11 12:57:04 -0500

    Primer commit

---
+++ Controller/Banco.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jpfra on 11/11/25.
+//
+
+#include "Banco.h"
---
+++ Controller/Banco.h
@@ -0,0 +1,13 @@
+//
+// Created by jpfra on 11/11/25.
+//
+
+#ifndef PROYECTOFINALPOO_BANCO_H
+#define PROYECTOFINALPOO_BANCO_H
+
+
+class Banco {
+};
+
+
+#endif //PROYECTOFINALPOO_BANCO_H
