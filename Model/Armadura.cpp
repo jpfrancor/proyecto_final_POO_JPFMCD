13487f63616645a16e608efa482ee8d60d570bfa
martin <martin.cnz@icloud.com>
2025-11-19 17:19:52 -0500

    implementar clase Armadura

---
+++ Model/Armadura.cpp
@@ -0,0 +1,35 @@
+//
+// Created by marti on 19/11/2025.
+//
+
+#include "Armadura.h"
+#include "Entidad.h"
+#include <iostream>
+
+// Constructor
+
+Armadura::Armadura(std::string nombre, int defensaExtra)
+    : Item(nombre, "Una pieza de equipo que protege del daño.", true),
+      defensaAdicional(defensaExtra)
+{
+}
+
+void Armadura::usar(Entidad* objetivo) {
+    if (objetivo != nullptr) {
+
+        std::cout << objetivo->getNombre() << " se ha equipado " << this->getNombre() << "." << std::endl;
+
+        // === LÓGICA PRINCIPAL ===
+
+
+        objetivo->modificarEstadisticas(0, 0, this->defensaAdicional);
+
+
+        std::cout << "Defensa aumentada en +" << this->defensaAdicional
+                  << ". (Defensa Total: " << objetivo->getDefensa() << ")" << std::endl;
+    }
+}
+
+int Armadura::getDefensaAdicional() const {
+    return defensaAdicional;
+}
