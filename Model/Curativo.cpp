c6ea8cb86d0a6e271bb8d01af51e55a1fa34fbc9
martin <martin.cnz@icloud.com>
2025-11-19 15:36:45 -0500

    Implementación de la clase curativo

---
+++ Model/Curativo.cpp
@@ -0,0 +1,28 @@
+//
+// Created by marti on 19/11/2025.
+//
+
+#include "Curativo.h"
+#include "Heroe.h" // ¡Ahora incluimos Heroe.h!
+#include <iostream>
+
+// ... (Constructor)
+
+void Curativo::usar(Entidad& usuario) {
+    // 1. Intentar convertir la referencia de Entidad a Heroe
+    Heroe* heroe = dynamic_cast<Heroe*>(&usuario);//Nos permite usar métodos que solo existen en la clase derivada
+                                                  //(como curarse() en Heroe)
+                                                  //cuando recibes un objeto genérico (como Entidad).
+
+    // 2. Comprobar que la conversión fue exitosa
+    if (heroe) {
+        std::cout << heroe->getNombre() << " ha usado " << this->getNombre()
+                  << " y ha recuperado " << this->valorCuracion << " HP." << std::endl;
+
+        // **Llamada al metodo específico del Héroe**
+        heroe->curarse(this->valorCuracion);
+    } else {
+        std::cout << "ERROR: Solo el Heroe puede usar items curativos." << std::endl;
+    }
+}
+}
